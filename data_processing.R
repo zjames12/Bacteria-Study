e33305714c2119b9fdc2a9320f384348761c5584
zjames12 <zjames7@gatech.edu>
2019-07-08 15:20:24 -0400

    removed duplicates

--- data_processing.R
+++ data_processing.R
@@ -4,18 +4,12 @@ library(reshape2)
 
 load("../Bacteria_Data/Data from Backdive-2.Rdata")
 df <- merge
-df <- df[0:10000,]
+# df <- df[0:1873223,]
 
-Mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
 
-df <- df %>% distinct(bacdive_id, section, subsection, field, species, .keep_all = F)
+df <- df %>% distinct(bacdive_id, section, subsection, field, species, .keep_all = T)
 
-# q <- dcast(df, bacdive_id~section+subsection+field, fun.aggregate = Mode, drop = F)
 q <- dcast(df, bacdive_id~section+subsection+field+species)
-# save(q, file = "../Bacteria_Data/Data from Backdive-2 Processed.Rdata")
 write.csv(q, file = "data.csv", row.names = F)
 
 # remove columns with near zero variance Global
@@ -25,5 +19,3 @@ dropnzv<-names(q[ , which(names(q) %in% nzv)])
 q<-q[ , -which(names(q) %in% nzv)]
 write.csv(q, file = "dropnzvdata.csv", row.names = F)
 
-dmy <- dummyVars(" ~ .", data = q)
-trsf <- data.frame(predict(dmy, newdata = q))
